ba639b485c63231f4e19db37f1351e4fefe34232
TheElyphant <eodagba@outlook.com>
2024-01-28 20:53:45 +0100

    Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-for tens in range(0, 10):
+for tens in range(10):
     for units in range(tens + 1, 10):
-        if tens == 0 and units == 1:
+        if tens == 8 and units == 9:
             print("{}{}".format(tens, units))
         else:
-            print("{:02d}, ".format(tens * 10 + units), end='')
+            print("{:02d}".format(tens * 10 + units), end=', ')
 print()
